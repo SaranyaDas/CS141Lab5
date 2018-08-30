b83c946b510ca9784680b78d842f563810e85958
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-08-31 03:23:02 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a, b;
+cout <<"Enter two numbers: "<< endl;
+cin >> a >> b;
+cout <<"The maximum is ";
+if (a>b) {
+cout <<a<< "."<< endl;
+}
+else {
+cout <<b<< "."<< endl;
+}
+return 0;
+}
