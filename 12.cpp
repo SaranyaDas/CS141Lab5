017c488df89816d6efc6bb1eabd4cc0f081d98bb
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-08-31 09:11:40 +0530

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -6,14 +6,14 @@ int main() {
 int x;
 cout <<"To find the number of days in a month."<< endl;
 //Asking for input
-cout <<"Enter month number: "<< endl;
+cout <<"Enter month number: ";
 cin >> x;
 //Displaying result
 cout <<"The month has ";
 if (x==2) {
 cout <<"28 or 29 days, depending on the year."<< endl;
 }
-else { if (x==4 or x==6 or x==9 or x==11) {
+else { if (x==4 || x==6 || x==9 || x==11) {
 cout <<"30 days."<< endl;
 }
 else {
