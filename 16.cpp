ec6c4e29cbb9291f0ea603762f2fa12cfeeeb486
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-08-31 09:12:53 +0530

    Update 16.cpp

--- 16.cpp
+++ 16.cpp
@@ -6,15 +6,15 @@ int main() {
 int a, b, c;
 cout <<"To find out whether a triangle is equilateral, isosceles or scalene."<< endl;
 //Asking for input
-cout <<"Enter lengths of three sides of triangle: "<< endl;
+cout <<"Enter lengths of three sides of triangle: ";
 cin >> a >> b >> c;
 //Displaying result
 cout <<"The triangle is ";
-if (a==b and b==c and c==a) {
+if (a==b && b==c && c==a) {
 cout <<"equilateral."<< endl;
 }
 else {
-if (a==b or b==c or c==a) {
+if (a==b || b==c || c==a) {
 cout <<"isoceles."<< endl;
 }
 else {
