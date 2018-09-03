26fd9a8ec35d9a5ede17dc2793776fa7ad31502a
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-09-03 09:28:51 +0530

    Update 31.cpp

--- 31.cpp
+++ 31.cpp
@@ -1,28 +1,20 @@
 //Library
 #include <iostream>
 using namespace std;
-//Counting number of digits in given number
+//Checking whether given number is prime
 int main() {
-int n, i, count = 0;
-cout <<"To count the number of digits in a number."<< endl;
+int n, i;
+cout <<"To check whether a number is prime or not."<< endl;
 //Asking for input
 cout << "Enter a number: ";
 cin >> n;
 //Displaying result
-if (n == 0) {
-cout << n << " is not prime";
-}
-else {
-for(i=2; i < n; i++)
-if (n % i == 0) {
-count++;
-}
-if (count > 1) {
+for(i = 2; i <= n/2; ++i) { 
+if(n%i == 0) {
 cout << n << " is not prime.";
 }
 else {
 cout << n << " is prime.";
-}
 }
 return 0;
 }
