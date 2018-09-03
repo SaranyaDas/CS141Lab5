3296545829668218071df3537a6f03eb2385c890
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-09-03 09:34:21 +0530

    Update 31.cpp

--- 31.cpp
+++ 31.cpp
@@ -4,17 +4,21 @@ using namespace std;
 //Checking whether given number is prime
 int main() {
 int n, i;
+bool isPrime = true;
 cout <<"To check whether a number is prime or not."<< endl;
 //Asking for input
 cout << "Enter a number: ";
 cin >> n;
 //Displaying result
-for(i = 2; i <= n/2; ++i) { 
-if(n%i == 0) {
-cout << n << " is not prime.";
+for(i = 2; i <= n / 2; ++i) {
+if (n % i == 0) {
+isPrime = false;
+break;
 }
-else {
-cout << n << " is prime.";
 }
+if (isPrime)
+cout << n << " is a prime number.";
+else
+cout << n << " is not a prime number.";
 return 0;
 }
